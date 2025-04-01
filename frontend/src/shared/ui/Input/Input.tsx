44560682cecf968c3a78979a8dffe2241ae3e5a4
Serdeshnow <vserdes1977@mail.ru>
2025-04-01 16:39:09 +0300

    [feat](project): commit

--- frontend/src/shared/ui/Input/Input.tsx
+++
@@ -1,8 +0,0 @@
-import React, { InputHTMLAttributes } from 'react';
-import { Input as AndDInput } from 'antd';
-
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-export const Input: React.FC<InputProps> = (props) => {
-  return <AndDInput {...props} />;
-};
