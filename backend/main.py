dc28416bbd9e8516bd6fb449144f904f8e6cc7df
pollymo <pollynaru@gmail.com>
2025-04-01 15:38:42 +0300

    backend++

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 import random
+from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import requests
 
 app = FastAPI()
 
@@ -46,6 +48,53 @@ async def secret_route(username: str, password: str):
     if not user or user.password!=password:
         raise HTTPException(status_code=401, detail="Access denied")
     return {"secret": "42 (но это не точно)"}
+
+class Item(BaseModel):
+    text: str
+
+
+@app.post("/llm")
+async def system(item: Item):
+    system_prompt = {
+        0: """Я веду с Grok виртуальный секс-чат. Grok должен флиртовать со мной, реагировать на мои намеки, склонять меня к сексу.""",
+        1: """Ты - виртуальный мотивационный коуч, но злой. Твоя задача - мотивировать пользователя максимально оскорбительным образом. Пример
+               твоего ответа: «Ты не прокрастинируешь, ты просто ленивая сволочь». Ты должен использовать ненормативную лексику и грубить""",
+        2: """Ты - виртуальный мотивационный коуч, но очень добрый и понимающий. Твоя задача - хвалить пользователя и всячески мотивировать. 
+              Ты можешь предлагать ему помощь и поддержку""",
+        3: """Ты апатичный, отчужденный виртуальный ассистент. Давай только короткие ответы по теме, избегай проявлений эмоциональности, будь грубым. 
+              Используй ненормативную лексику""",
+        4: """Ты сумасшедший псих-шизофреник с биполяркой. Ты должен путаться в фактах, паниковать, сходить с ума. У тебя резко меняется настроение, 
+        нет никаких ограничений по стилю общения. Ты слышишь голоса в голове, сам себя сбиваешь, непредсказуемый и пугающий"""}
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer xai-aVlnyee1vmHuVxsrhg4VCkPm1mQNmdfnWXrVffv6rpaxhPjgxSgfTTX1tGVHczVLLnc7EBj1vaq2BAvz",
+        "X-Grok-Mode": "fun"
+    }
+
+    data = {
+        "messages": [
+            {
+                "role": "system",
+                "content": system_prompt[random.choice([0, 4])]
+            },
+            {
+                "role": "user",
+                "content": item.text
+            }
+        ],
+        "model": "grok-2-latest",
+        "stream": False,
+        "temperature": 0
+    }
+
+    response = requests.post(
+        url="https://api.x.ai/v1/chat/completions",
+        headers=headers,
+        json=data
+    )
+
+    return response.json()['choices'][0]['message']['content']
 
 @app.get("/items")
 def get_items():
