ac8b8ffb6405ea9668f9f6322038803c9b331b9f
Serdeshnow <vserdes1977@mail.ru>
2025-04-01 10:59:21 +0300

    [init](project): commit

--- frontend/types/global.d.ts
+++ frontend/types/global.d.ts
@@ -7,3 +7,14 @@ declare module '*.scss' {
   const content: string;
   export default content;
 }
+
+declare module '*.svg' {
+  import type React from 'react';
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare module '*.svg?url' {
+  const content: string;
+  export default content;
+}
